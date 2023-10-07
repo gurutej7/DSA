d2e52fbdce0f87b0c7225d548568820f0455adf8
gurutej7 <ngurutej47@gmail.com>
2023-10-07 12:30:59 +0530

    37. Sudoku Solver

--- Backtracking/sudokuSolver.java
+++ Backtracking/sudokuSolver.java
@@ -9,7 +9,6 @@ A sudoku solution must satisfy all of the following rules:
 2) Each of the digits 1-9 must occur exactly once in each column.
 3) Each of the digits 1-9 must occur exactly once in each of the 9 3x3 sub-boxes of the grid.
 The '.' character indicates empty cells.
-
  */
 
 public class sudokuSolver {
