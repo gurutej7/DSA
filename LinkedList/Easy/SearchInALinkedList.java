2573e0f69e2f2c6cdc38b1ff209a19fff7d24a9c
gurutej7 <ngurutej47@gmail.com>
2023-11-09 17:38:46 +0530

    Search in a Linked List

--- LinkedList/Easy/SearchInALinkedList.java
+++ LinkedList/Easy/SearchInALinkedList.java
@@ -1,12 +1,36 @@
 package LinkedList.Easy;
-
+/*
+ *  Given a Linked List and a value to search 
+ * if the value is present in the list return true else false
+ */
 
 public class SearchInALinkedList {
-
+    
     public static void main(String[] args) {
         LL list = new LL();
 
         list.addFirst(1);
+        list.add(10);
+        list.add(20);
+        list.add(35);
+        list.add(45);
+        
+
+        System.out.println(searchInLinkedList(list.head, 35));
+
+        System.out.println(searchInLinkedList(list.head, 50));
+    }
+
+    public static boolean searchInLinkedList(LL.Node head, int k)
+    {
+        // Write Your Code Here.
+        if(head == null) return false;
+        LL.Node curr = head;
+        while(curr!=null){
+            if(curr.data==k) return true;
+            curr = curr.next;
+        }
+        return false;
     }
     
 }
