d586fe806c95e6576beebdc71812784570211131
gurutej7 <ngurutej47@gmail.com>
2023-11-10 20:40:24 +0530

    Middle of the LinkedList

--- LinkedList/Medium/MiddleOfTheLL.java
+++ LinkedList/Medium/MiddleOfTheLL.java
@@ -5,7 +5,10 @@ import LinkedList.Easy.*;
 /* 876. Middle of the Linked List
     Given the head of a singly linked list, return the middle node of the linked list.
     If there are two middle nodes, return the second middle node.
-
+    Example 1:
+    Input: head = [1,2,3,4,5]
+    Output: [3,4,5]
+    Explanation: The middle node of the list is node 3.
 */
 
 public class MiddleOfTheLL{
@@ -14,7 +17,7 @@ public class MiddleOfTheLL{
 
         //Create a Linked List 
         LL list = new LL();
-        // add elements to the LinkedList
+        // add nodes to the LinkedList
         list.add(1);
         list.add(2);
         list.add(3);
@@ -30,19 +33,23 @@ public class MiddleOfTheLL{
         ans.printListNode(ans);
         
     }
-
+    /*
+      we increment slow ptr by 1 and fast ptr by 2, so if take a close look fast ptr will travel double that of 
+      the slow pointer. So when the fast ptr will be at the end of the Linked List, slow ptr would have covered 
+      half of the Linked List till then. So slow ptr will be pointing towards the middle of Linked List.
+     */
     public static LL.Node middleNode(LL.Node head) {
         if(head==null || head.next==null){
             return head;
         }
         LL.Node sp = head;
         LL.Node fp = head;
-         while(fp != null && fp.next!= null && fp.next.next!=null ){
+         while(fp != null && fp.next!= null ){
             sp = sp.next ;
             fp = fp.next.next ;
         }
-        if(fp.next !=null) return sp.next;
-        else return sp;
+        
+        return sp;
         
     }
 }
