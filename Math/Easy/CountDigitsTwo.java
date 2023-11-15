72a5a7deb22d0ce4b99bd223a64faf2b350c9642
gurutej7 <ngurutej47@gmail.com>
2023-11-15 11:05:40 +0530

    Count Digits - 2

--- Math/Easy/CountDigitsTwo.java
+++ Math/Easy/CountDigitsTwo.java
@@ -1,5 +1,36 @@
 package Math.Easy;
 
+/*
+You are Given a Number 'n'
+Find the Number of digits of 'n' that evenly divide 'n' 
+
+*A digit evenly divides 'n' if it leaves no remainder when dividing 'n'.
+
+Example : n = 336  output = 3   Explanation : 3,3,6 all digits evenly divide n
+Example : n = 35   output = 1   Explanation : only 5 evenly divides n
+ */
+
 public class CountDigitsTwo {
+    public static void main(String[] args) {
+
+        int n = 336;
+
+        System.out.println(countDigits(n));
+        
+    }
+
+    public static int countDigits(int n){
+        // Write your code here.
+        int cnt = 0;
+        //Make a Copy of N in which the value will be changing
+        int copyN = n ;
+        while(copyN >0){
+            int lastDigit = copyN%10;
+            //check the condition for the current digit
+            if(lastDigit != 0 && n % lastDigit == 0) cnt++;
+            copyN /= 10;
+        }
+        return cnt;
+    }
     
 }
